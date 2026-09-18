b57fb3780884bbd551a0b850cf1e64538722d6d3
elieledmilson-crypto <eliel.edmilson@academico.ifpb.edu.br>
2026-09-17 21:28:46 -0300

    Add Supabase property onboarding route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -110,10 +110,12 @@ import AlertsPage from '../pages/alerts/AlertsPage'
 import ReportsPage from '../pages/reports/ReportsPage'
 import PropertyMapPage from '../pages/map/PropertyMapPage'
 import IntelligencePage from '../pages/intelligence/IntelligencePage'
+import PropertySetupPage from '../pages/PropertySetupPage'
 
 import { ProtectedRoute } from '../components/common/ProtectedRoute'
 import { PublicOnlyRoute } from '../components/common/PublicOnlyRoute'
 import { PermissionRoute } from '../components/common/PermissionRoute'
+import { PropertyReadyRoute } from '../components/common/PropertyReadyRoute'
 
 export default function AppRoutes() {
   return (
@@ -123,7 +125,13 @@ export default function AppRoutes() {
       </Route>
 
       <Route element={<ProtectedRoute />}>
-        <Route element={<AppLayout />}>
+        <Route
+          path="/configuracao-inicial"
+          element={<PropertySetupPage />}
+        />
+
+        <Route element={<PropertyReadyRoute />}>
+          <Route element={<AppLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
 
           <Route path="/acesso-negado" element={<AccessDeniedPage />} />
@@ -472,6 +480,7 @@ export default function AppRoutes() {
             />
           </Route>
         </Route>
+      </Route>
       </Route>
 
       <Route
